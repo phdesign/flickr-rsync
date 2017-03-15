c1d1cb5ce29f5e71f1fd1d4d77eeede602dc4a6d
Paul Heasley <paul@phdesign.com.au>
2017-03-16 08:44:10 +1100

    Reading keys from config

--- flickr-syncp.py
+++ flickr-syncp.py
@@ -1,13 +1,31 @@
 import os, sys
-sys.path.append(os.path.dirname(__file__) + "/libs")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/libs')
 import flickr_api
 from flickr_api.api import flickr
+import ConfigParser
 
-flickr_api.set_keys(api_key = '', api_secret = '')
+class Config(object):
 
-print flickr.reflection.getMethodInfo(method_name = "flickr.photos.getInfo")
-# flickr_api.set_auth_handler(filename)
-# user = flickr_api.test.login()
-# print user
+    def read(self):
+        config = ConfigParser.SafeConfigParser()
+        config.read(os.path.splitext(os.path.abspath(__file__))[0] + ".ini")
 
-print "done"
+        self.flickr = dict(config.items('Flickr'))
+
+class Program(object):
+    
+    def main(self):
+        config = Config()
+        config.read()
+
+        flickr_api.set_keys(api_key = config.flickr['api_key'], api_secret = config.flickr['api_secret'])
+
+        print flickr.reflection.getMethodInfo(method_name = "flickr.photos.getInfo")
+        # flickr_api.set_auth_handler(filename)
+        # user = flickr_api.test.login()
+        # print user
+
+        print "done"
+
+if __name__ == "__main__":
+    Program().main()
