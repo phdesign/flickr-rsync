a6f2c4f095710f2d8288766d22e1c7d69c6af932
Paul Heasley <paul@phdesign.com.au>
2017-05-02 07:44:57 +1000

    Improved messaging from shallow sync

--- modules/sync.py
+++ modules/sync.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-
+import os
 import operator
+import time
 
 class Sync(object):
     
@@ -8,14 +8,18 @@ class Sync(object):
         self._config = config
         self._src = src
         self._dest = dest
+        self._file_count = 0
 
     def run(self):
+        start = time.time()
         self._src_folders = self._src.list_folders()
         self._dest_folders = self._dest.list_folders()
         self._copy_shallow()
+        self._print_summary(time.time() - start)
 
     def _copy_shallow(self):
         for src_folder in self._src_folders:
+            print src_folder.name + os.sep
             exists = False
             for dest_folder in self._dest_folders:
                 if src_folder.name == dest_folder.name:
@@ -28,7 +32,12 @@ class Sync(object):
         pass
 
     def _copy_folder(self, folder):
-        print "Copying folder " + folder.name
         files = self._src.list_files_in_folder(folder)
         for file_to_copy in files:
-            self._src.copy_file(file_to_copy, folder.name, self._dest)
+            print os.path.join(folder.name, file_to_copy.name)
+            self._file_count += 1
+            if not self._config.dry_run:
+                self._src.copy_file(file_to_copy, folder.name, self._dest)
+
+    def _print_summary(self, elapsed):
+        print "\ntransferred {} file(s) in {} sec".format(self._file_count, round(elapsed, 2))
--- test/sync_test.py
+++ test/sync_test.py
@@ -14,44 +14,39 @@ class SyncTest(unittest.TestCase):
         self.src_storage = MagicMock()
         self.dest_storage = MagicMock()
         self.sync = Sync(self.config, self.src_storage, self.dest_storage)
+        self.folder_one = FolderInfo(id=1, name='A')
+        self.folder_two = FolderInfo(id=2, name='B')
+        self.folder_three = FolderInfo(id=3, name='C')
+        self.folder_four = FolderInfo(id=4, name='D')
 
     def test_should_call_copy_folder_for_each_missing_folder_in_src(self):
-        folder_one = FolderInfo(id=1, name='A')
-        folder_two = FolderInfo(id=2, name='B')
-        folder_three = FolderInfo(id=3, name='B')
-        self.src_storage.list_folders.return_value = [folder_one, folder_two, folder_three]
+        self.src_storage.list_folders.return_value = [self.folder_one, self.folder_two, self.folder_three]
         self.dest_storage.list_folders.return_value = []
 
         mock = MagicMock()
         self.sync._copy_folder = mock
         self.sync.run()
 
-        mock.assert_any_call(folder_one)
-        mock.assert_any_call(folder_two)
-        mock.assert_any_call(folder_three)
+        mock.assert_any_call(self.folder_one)
+        mock.assert_any_call(self.folder_two)
+        mock.assert_any_call(self.folder_three)
         self.assertEqual(mock.call_count, 3)
 
     def test_should_call_copy_folder_for_each_missing_folder_given_some_exist_already(self):
-        folder_one = FolderInfo(id=1, name='A')
-        folder_two = FolderInfo(id=2, name='B')
-        folder_three = FolderInfo(id=3, name='C')
-        folder_four = FolderInfo(id=4, name='D')
-        self.src_storage.list_folders.return_value = [folder_one, folder_two, folder_three, folder_four]
-        self.dest_storage.list_folders.return_value = [folder_four, folder_three]
+        self.src_storage.list_folders.return_value = [self.folder_one, self.folder_two, self.folder_three, self.folder_four]
+        self.dest_storage.list_folders.return_value = [self.folder_four, self.folder_three]
 
         mock = MagicMock()
         self.sync._copy_folder = mock
         self.sync.run()
 
-        mock.assert_any_call(folder_one)
-        mock.assert_any_call(folder_two)
+        mock.assert_any_call(self.folder_one)
+        mock.assert_any_call(self.folder_two)
         self.assertEqual(mock.call_count, 2)
 
     def test_not_should_call_copy_folder_given_all_exist_already(self):
-        folder_one = FolderInfo(id=1, name='A')
-        folder_two = FolderInfo(id=2, name='B')
-        self.src_storage.list_folders.return_value = [folder_one, folder_two]
-        self.dest_storage.list_folders.return_value = [folder_two, folder_one]
+        self.src_storage.list_folders.return_value = [self.folder_one, self.folder_two]
+        self.dest_storage.list_folders.return_value = [self.folder_two, self.folder_one]
 
         mock = MagicMock()
         self.sync._copy_folder = mock
