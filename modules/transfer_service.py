364894e7cc5a544ec3d1d28946687840759ee6da
Paul Heasley <paul@phdesign.com.au>
2017-05-01 20:43:27 +1000

    Rewrote transfer service

--- modules/transfer_service.py
+++ modules/transfer_service.py
@@ -6,29 +6,23 @@ class TransferService(object):
 
     def copy_file(file_info, src_storage, folder_name, dest_storage):
 
-        if isinstance(dest_storage, LocalStorage):
-            dest_path = os.path.join(dest_storage.path, folder_name, file_info.name)
-        elif isinstance(dest_storage, FlickrStorage):
-            temp_file = NamedTemporaryFile()
-            dest_path = temp_file.name
-        else:
-            raise NotImplementedError()
-
         if isinstance(src_storage, LocalStorage):
-            src_storage.copy_file(file_info, dest_path)
+            src = file_info.full_path
+            if isinstance(dest_storage, LocalStorage):
+                dest = os.path.join(dest_storage.path, folder_name, file_info.name)
+                dest_storage.copy_file(src, dest)
+
+            elif isinstance(dest_storage, FlickrStorage):
+                dest_storage.upload(src_path, folder_name, file_info.name)
+
         elif isinstance(src_storage, FlickrStorage):
-            src_storage.download(file_info, dest_path)
-        else:
-            raise NotImplementedError()
+            if isinstance(dest_storage, LocalStorage):
+                dest = os.path.join(dest_storage.path, folder_name, file_info.name)
+                src_storage.download(file_info, dest)
 
-        if isinstance(dest_storage, LocalStorage):
-            # We're done
-            pass
-        elif isinstance(dest_storage, FlickrStorage):
-            dest_storage.upload(dest_path, folder_name, file_name)
-            if temp_file:
+            elif isinstance(dest_storage, FlickrStorage):
+                temp_file = NamedTemporaryFile()
+                src_storage.download(file_info, temp_file.name)
+                dest_storage.upload(temp_file.name, folder_name, file_info.name)
                 temp_file.close()
-        else:
-            raise NotImplementedError()
-
 
