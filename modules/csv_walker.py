8f0cc5e93a53f002c11d4c3948bd51c3b78c0ca8
Paul Heasley <paul@phdesign.com.au>
2017-05-05 22:12:38 +1000

    Working on CsvWalker Rx

--- modules/csv_walker.py
+++ modules/csv_walker.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import operator
 from walker import Walker
+from rx import Observable
 
 class CsvWalker(Walker):
     
@@ -9,13 +10,25 @@ class CsvWalker(Walker):
         self._storage = storage
 
     def walk(self):
-        folders = self._storage.list_folders()
-        if self._config.list_sort:
-            folders = sorted(folders, key=lambda x: x.name)
         print("Folder, Filename, Checksum")
-        if self._config.root_files:
-            self._print_root_files()
-        self._print_folders(folders)
+
+        # folders = Observable.from_(self._storage.list_folders()).publish().auto_connect(2)
+        # root_files = Observable.from_((x, None) for x in self._storage.list_files(None))
+        # folders.subscribe(lambda folder: print("{}".format(folder.name)))
+        # folders.flat_map(lambda folder: ((x, folder) for x in self._storage.list_files(folder))) \
+            # .start_with(root_files) \
+            # .subscribe(lambda (x, folder): print("{}, {}, {}".format(folder.name, x.name.encode('utf-8'), x.checksum)))
+
+        Observable.from_(self._storage.list_folders()) \
+            .flat_map(lambda folder: ((x, folder) for x in self._storage.list_files(folder))) \
+            .subscribe(lambda (x, folder): print("{}, {}, {}".format(folder.name, x.name.encode('utf-8'), x.checksum)))
+
+        # if self._config.list_sort:
+            # folders = sorted(folders, key=lambda x: x.name)
+        # print("Folder, Filename, Checksum")
+        # if self._config.root_files:
+            # self._print_root_files()
+        # self._print_folders(folders)
 
     def _print_root_files(self):
         files = self._storage.list_files(None)
