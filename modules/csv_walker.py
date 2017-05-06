8e0c3b2309af6070abc6f73c5c18a61e6f35f1c6
Paul Heasley <paul@phdesign.com.au>
2017-05-06 21:46:00 +1000

    Aaaand root files are working

--- modules/csv_walker.py
+++ modules/csv_walker.py
@@ -14,14 +14,14 @@ class CsvWalker(Walker):
         source = Observable.from_(self._storage.list_folders()) \
             .flat_map(lambda folder: ((fileinfo, folder) for fileinfo in self._storage.list_files(folder)))
         if self._config.root_files:
-           source.start_with(self._storage.list_files(None)) 
+           source = source.start_with(*[(fileinfo, None) for fileinfo in self._storage.list_files(None)]) 
         if self._config.list_sort:
-            source.to_sorted_list(key_selector=lambda (fileinfo, folder): (folder.name, fileinfo.name)) \
+            source.to_sorted_list(key_selector=lambda (fileinfo, folder): (folder.name if folder else '', fileinfo.name)) \
                 .subscribe(lambda items: [self._print_file(folder, fileinfo) for fileinfo, folder in items])
         else:
             source.subscribe(lambda (fileinfo, folder): self._print_file(folder, fileinfo))
     
     def _print_file(self, folder, fileinfo):
-        print("{}, {}, {}".format(folder.name, fileinfo.name.encode('utf-8'), fileinfo.checksum))
+        print("{}, {}, {}".format(folder.name if folder else '', fileinfo.name.encode('utf-8'), fileinfo.checksum))
 
 
