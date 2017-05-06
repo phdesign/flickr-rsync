49855b20dff288aae55e8be4ecd803f731d3c203
Paul Heasley <paul@phdesign.com.au>
2017-05-06 21:35:20 +1000

    Sorted CsvWalker working well

--- modules/csv_walker.py
+++ modules/csv_walker.py
@@ -11,13 +11,15 @@ class CsvWalker(Walker):
     def walk(self):
         print("Folder, Filename, Checksum")
 
-        folders = Observable.from_(self._storage.list_folders()) \
+        source = Observable.from_(self._storage.list_folders()) \
             .flat_map(lambda folder: ((fileinfo, folder) for fileinfo in self._storage.list_files(folder)))
+        if self._config.root_files:
+           source.start_with(self._storage.list_files(None)) 
         if self._config.list_sort:
-            folders.to_sorted_list(key_selector=lambda (fileinfo, folder): (folder.name, fileinfo.name)) \
+            source.to_sorted_list(key_selector=lambda (fileinfo, folder): (folder.name, fileinfo.name)) \
                 .subscribe(lambda items: [self._print_file(folder, fileinfo) for fileinfo, folder in items])
         else:
-            folders.subscribe(lambda (fileinfo, folder): self._print_file(folder, fileinfo))
+            source.subscribe(lambda (fileinfo, folder): self._print_file(folder, fileinfo))
     
     def _print_file(self, folder, fileinfo):
         print("{}, {}, {}".format(folder.name, fileinfo.name.encode('utf-8'), fileinfo.checksum))
