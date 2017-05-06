f550e2f9e2ca29556fb802905f97c9d33d60ec65
Paul Heasley <paul@phdesign.com.au>
2017-05-06 21:48:45 +1000

    A better way to include root files in CsvWalker

--- modules/csv_walker.py
+++ modules/csv_walker.py
@@ -11,15 +11,15 @@ class CsvWalker(Walker):
     def walk(self):
         print("Folder, Filename, Checksum")
 
-        source = Observable.from_(self._storage.list_folders()) \
-            .flat_map(lambda folder: ((fileinfo, folder) for fileinfo in self._storage.list_files(folder)))
+        folders = Observable.from_(self._storage.list_folders())
         if self._config.root_files:
-           source = source.start_with(*[(fileinfo, None) for fileinfo in self._storage.list_files(None)]) 
+           folders = folders.start_with(None) 
+        files = folders.flat_map(lambda folder: ((fileinfo, folder) for fileinfo in self._storage.list_files(folder)))
         if self._config.list_sort:
-            source.to_sorted_list(key_selector=lambda (fileinfo, folder): (folder.name if folder else '', fileinfo.name)) \
+            files.to_sorted_list(key_selector=lambda (fileinfo, folder): (folder.name if folder else '', fileinfo.name)) \
                 .subscribe(lambda items: [self._print_file(folder, fileinfo) for fileinfo, folder in items])
         else:
-            source.subscribe(lambda (fileinfo, folder): self._print_file(folder, fileinfo))
+            files.subscribe(lambda (fileinfo, folder): self._print_file(folder, fileinfo))
     
     def _print_file(self, folder, fileinfo):
         print("{}, {}, {}".format(folder.name if folder else '', fileinfo.name.encode('utf-8'), fileinfo.checksum))
