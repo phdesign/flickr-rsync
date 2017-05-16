385ab8a4e324aabc60ca206ade0fd790f9fae41d
Paul Heasley <paul@phdesign.com.au>
2017-05-16 18:04:09 +1000

    Stuff

--- modules/sync.py
+++ modules/sync.py
@@ -6,7 +6,7 @@ from rx import Observable
 from rx.core import Scheduler
 from threading import current_thread
 from functools import partial
-from rx.concurrency import ThreadPoolScheduler
+from rx.concurrency import ThreadPoolScheduler, EventLoopScheduler
 
 class Sync(object):
     
@@ -21,50 +21,16 @@ class Sync(object):
         if self._config.dry_run:
             print("dry run enabled, no files will be copied")
         print("building folder list...")
-        self.run_start()
-
-    def run_subscribe(self):
         start = time.time()
 
-        def done():
-            for (src, dest) in to_merge:
-                self._merge_folders(src, dest)
-            print("done. thread: " + current_thread().name)
-
+        main_thread_scheduler = EventLoopScheduler(lambda x: current_thread())
         src_folders = Observable.from_(self._src.list_folders())
         dest_folders = {folder.name.lower(): folder for folder in self._dest.list_folders()}
-        to_merge = []
-        if self._config.root_files:
-           src_folders = src_folders.start_with(None)
-        src_folders.subscribe(
-            on_next=lambda folder: to_merge.append((folder, dest_folders[folder.name.lower()])) \
-                if folder.name.lower() in dest_folders else self._copy_folder(folder),
-            on_completed=done)
-        self._print_summary(time.time() - start)
-
-    def run_start(self):
-        start = time.time()
-
-        def _copy_folder(folder):
-            print("copy " + folder.name + " thread: " + current_thread().name)
-            return Observable.from_(self._src.list_files(folder)) \
-                .flat_map(lambda f: Observable.start(lambda: self._copy_file(folder.name, f)))
-
-        def _merge_folders(src_folder, dest_folder):
-            print("merge " + folder.name + " thread: " + current_thread().name)
-            dest_files = [fileinfo.name.lower() for fileinfo in self._dest.list_files(dest_folder)]
-            return Observable.from_(self._src.list_files(src_folder)) \
-                .filter(lambda fileinfo: not fileinfo.name.lower() in dest_files) \
-                .flat_map(lambda f: Observable.start(lambda: self._copy_file(folder.name, f)))
-
-        src_folders = Observable.from_(self._src.list_folders())
-        dest_folders = {folder.name.lower(): folder for folder in self._dest.list_folders()}
-        to_merge = []
         if self._config.root_files:
            src_folders = src_folders.start_with(None)
         src_folders \
-            .flat_map(lambda f: _merge_folders(f, dest_folders[f.name.lower()]) \
-                if f.name.lower() in dest_folders else _copy_folder(f)) \
+            .flat_map(lambda f: self._merge_folders(f, dest_folders[f.name.lower()]) \
+                if f.name.lower() in dest_folders else self._copy_folder(f)) \
             .observe_on(Scheduler.event_loop) \
             .to_blocking() \
             .subscribe(
@@ -75,51 +41,17 @@ class Sync(object):
         self._print_summary(time.time() - start)
         time.sleep(2)
 
-    def run_observe_on(self):
-        start = time.time()
-
-        def _copy_folder(folder):
-            print("copy " + folder.name + " thread: " + current_thread().name)
-            return Observable.from_(self._src.list_files(folder)) \
-                .observe_on(self._pool_scheduler) \
-                .flat_map(partial(self._copy_file, folder.name))
-
-        def _merge_folders(src_folder, dest_folder):
-            print("merge " + folder.name + " thread: " + current_thread().name)
-            dest_files = [fileinfo.name.lower() for fileinfo in self._dest.list_files(dest_folder)]
-            return Observable.from_(self._src.list_files(src_folder)) \
-                .filter(lambda fileinfo: not fileinfo.name.lower() in dest_files) \
-                .observe_on(self._pool_scheduler) \
-                .flat_map(partial(self._copy_file, folder.name))
-
-        src_folders = Observable.from_(self._src.list_folders())
-        dest_folders = {folder.name.lower(): folder for folder in self._dest.list_folders()}
-        to_merge = []
-        if self._config.root_files:
-           src_folders = src_folders.start_with(None)
-        src_folders \
-            .flat_map(lambda f: _merge_folders(f, dest_folders[f.name.lower()]) \
-                if f.name.lower() in dest_folders else _copy_folder(f)) \
-            .observe_on(Scheduler.event_loop) \
-            .subscribe(on_completed=lambda: print("done. thread: " + current_thread().name))
-        
-        self._print_summary(time.time() - start)
-
     def _copy_folder(self, folder):
         print("copy " + folder.name + " thread: " + current_thread().name)
-        source = Observable.from_(self._src.list_files(folder)) \
-            .observe_on(self._pool_scheduler)
-        source.to_blocking().subscribe(partial(self._copy_file, folder.name))
-        return source
+        return Observable.from_(self._src.list_files(folder)) \
+            .flat_map(lambda f: Observable.start(lambda: self._copy_file(folder.name, f)))
 
     def _merge_folders(self, src_folder, dest_folder):
         print("merge " + folder.name + " thread: " + current_thread().name)
         dest_files = [fileinfo.name.lower() for fileinfo in self._dest.list_files(dest_folder)]
-        source = Observable.from_(self._src.list_files(src_folder)) \
+        return Observable.from_(self._src.list_files(src_folder)) \
             .filter(lambda fileinfo: not fileinfo.name.lower() in dest_files) \
-            .observe_on(self._pool_scheduler)
-        source.to_blocking().subscribe(partial(self._copy_file, src_folder.name))
-        return source
+            .flat_map(lambda f: Observable.start(lambda: self._copy_file(folder.name, f)))
 
     def _copy_file(self, folder_name, fileinfo):
         print(os.path.join(folder_name, fileinfo.name) + " thread: " + current_thread().name)
