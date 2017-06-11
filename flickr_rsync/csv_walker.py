af30810b1f23a41b44c62cb7431ddb91765cf447
Paul Heasley <paul@phdesign.com.au>
2017-06-11 21:46:08 +1000

    Adding --skip-files to csv

--- flickr_rsync/csv_walker.py
+++ flickr_rsync/csv_walker.py
@@ -13,22 +13,27 @@ class CsvWalker(Walker):
 
     def walk(self):
         start = time.time()
-        print("Folder, Filename, Checksum")
 
         # Create source stream
         folders = Observable.from_(self._storage.list_folders())
         if self._config.root_files:
            folders = folders.start_with(RootFolderInfo()) 
-        # Expand folder stream into file stream
-        files = folders.concat_map(lambda folder: Observable.from_((fileinfo, folder) for fileinfo in self._storage.list_files(folder)))
-        # Print each file
-        if self._config.list_sort:
-            files.to_sorted_list(key_selector=lambda (fileinfo, folder): "{} {}".format(folder.name, fileinfo.name)) \
-                .subscribe(on_next=lambda items: [self._print_file(folder, fileinfo) for fileinfo, folder in items],
-                    on_completed=lambda: self._print_summary(time.time() - start))
-        else:
-            files.subscribe(on_next=lambda (fileinfo, folder): self._print_file(folder, fileinfo),
+        if self._config.skip_files:
+            print("Folder")
+            folders.subscribe(on_next=lambda folder: print(folder.name) if folder else '',
                 on_completed=lambda: self._print_summary(time.time() - start))
+        else:
+            print("Folder, Filename, Checksum")
+            # Expand folder stream into file stream
+            files = folders.concat_map(lambda folder: Observable.from_((fileinfo, folder) for fileinfo in self._storage.list_files(folder)))
+            # Print each file
+            if self._config.list_sort:
+                files.to_sorted_list(key_selector=lambda (fileinfo, folder): "{} {}".format(folder.name, fileinfo.name)) \
+                    .subscribe(on_next=lambda items: [self._print_file(folder, fileinfo) for fileinfo, folder in items],
+                        on_completed=lambda: self._print_summary(time.time() - start))
+            else:
+                files.subscribe(on_next=lambda (fileinfo, folder): self._print_file(folder, fileinfo),
+                    on_completed=lambda: self._print_summary(time.time() - start))
     
     def _print_file(self, folder, fileinfo):
         print("{}, {}, {}".format(folder.name if folder else '', fileinfo.name, fileinfo.checksum))
